2e87a1da8d6b693770daa673c71e9b12458a4bd0
qianliyun <376084782@qq.com>
2022-11-26 19:17:26 +0800

    示例

---
+++ lib/example/index.ts
@@ -0,0 +1,16 @@
+/**
+ * @author qly
+ * @updateTime 2022/11/26
+ * @tip 一些说明blabla
+ */
+
+interface GameData {
+  lev: number
+}
+export default class example {
+  getRiddleByLev(lev: number, gameConfig: any): GameData {
+    return {
+      lev
+    }
+  }
+}
