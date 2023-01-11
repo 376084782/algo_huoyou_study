2b1787a1adff62b251b9deb51383817d365a2c0d
huan <15236283766@163.com>
2023-01-11 22:33:55 +0800

    feat: 4-6最优解

--- lib/module4_6/index.ts
+++ lib/module4_6/index.ts
@@ -4,13 +4,6 @@
 // 2. 参数范围：①堆数：2~4；
 // ②每堆数量：1~12
 // 3.过程记录：无
-// 二．练习模式
-// 一级：W2 或 L1
-// 二级：W3 或 L2
-// 三级：W4 或 L3
-// 四级：W5 或 L4
-// 五级：W6 或 L5
-// 【w1 是指全被取完的状态，L1 是指一步可达 W1 的状态】
 import RandomGenerater from '../util/RandomGenerater';
 
 interface GameData4_6 {
@@ -24,13 +17,13 @@ interface GameData4_6_action {
 }
 
 // 普通获取题目
-export const getRiddle = (stackNum?: number ): GameData4_6 => {
+export const getRiddle = (stackNum?: number): GameData4_6 => {
   const rg = new RandomGenerater(0)
   // 随机生成堆
-  if(stackNum && stackNum >= 2 && stackNum <= 4){
+  if (stackNum && stackNum >= 2 && stackNum <= 4) {
     let arr = [];
-    for(let i = 0; i < stackNum; i++){
-      arr.push(rg.RangeInteger(1,12));
+    for (let i = 0; i < stackNum; i++) {
+      arr.push(rg.RangeInteger(1, 12));
     }
     return {
       player: 1,
@@ -47,33 +40,33 @@ export const getRiddle = (stackNum?: number ): GameData4_6 => {
 
 // 判断题目是否合法
 export const checkRiddle = (dataDesk: GameData4_6): number => {
-  if(dataDesk.player != 1 && dataDesk.player != 2) return -1;
-  if(dataDesk.stack.length < 2 || dataDesk.stack.length > 4) return -1;
-  for(let i of dataDesk.stack){
-    if(i < 1 || i > 12) return -1;
+  if (dataDesk.player != 1 && dataDesk.player != 2) return -1;
+  if (dataDesk.stack.length < 2 || dataDesk.stack.length > 4) return -1;
+  for (let i of dataDesk.stack) {
+    if (i < 1 || i > 12) return -1;
   }
   return 1;
 }
 
 // 执行操作
 // 返回 -1不合法不可执行 0未结束 1先手获胜 2后手获胜
-export const doAction = (dataDesk: GameData4_6, dataAction: GameData4_6_action): {flagResult: number, dataResult: GameData4_6} => {
+export const doAction = (dataDesk: GameData4_6, dataAction: GameData4_6_action): { flagResult: number, dataResult: GameData4_6 } => {
   let result = checkAction(dataDesk, dataAction);
   let { stack = [], player } = dataDesk;
   let { index = 0, num = 0 } = dataAction;
-  if(result === -1){
+  if (result === -1) {
     return { flagResult: -1, dataResult: dataDesk };
   } else {
     let arr = stack;
     arr[index] = arr[index] - num;
-    if(arr[index] === 0){
+    if (arr[index] === 0) {
       arr.splice(index, 1);
     }
-    if(arr.length === 0){
+    if (arr.length === 0) {
       // 堆取完，获胜
-      return { flagResult: player, dataResult: {player, stack: []} };
+      return { flagResult: player, dataResult: { player, stack: [] } };
     } else {
-      return { flagResult: 0, dataResult: {player, stack: arr} };
+      return { flagResult: 0, dataResult: { player, stack: arr } };
     }
   }
 }
@@ -83,14 +76,14 @@ export const checkAction = (dataDesk: GameData4_6, dataAction: GameData4_6_actio
   let { stack = [], player } = dataDesk;
   let { index = 0, num = 0 } = dataAction;
   // 合法判断：非空堆，堆序号合法，玩家角色正确
-  if(
-    stack.length && 
-    index >= 0 && 
+  if (
+    stack.length &&
+    index >= 0 &&
     index < stack.length &&
-    ( player === 1 || player === 2 )
-  ){
+    (player === 1 || player === 2)
+  ) {
     let totalNum = stack[index];
-    if(totalNum % num === 0 && num <= totalNum && num > 0){
+    if (totalNum % num === 0 && num <= totalNum && num > 0) {
       return 1;
     } else {
       return -1;
@@ -104,7 +97,7 @@ export const checkAction = (dataDesk: GameData4_6, dataAction: GameData4_6_actio
 // 返回-1未结束 1先手获胜 2后手获胜
 export const checkDesk = (dataDesk: GameData4_6): number => {
   let { stack = [], player } = dataDesk;
-  if(stack.length === 0){
+  if (stack.length === 0) {
     return player;
   } else {
     return -1;
@@ -112,18 +105,134 @@ export const checkDesk = (dataDesk: GameData4_6): number => {
 }
 
 // 获取当前最佳应对策略，即机器人算法
-export const getActionAuto = (dataDesk: GameData4_6): {best: GameData4_6_action, nobest: GameData4_6_action} => {
+export const getActionAuto = (dataDesk: GameData4_6): { best: GameData4_6_action, nobest: GameData4_6_action } => {
   let { stack = [] } = dataDesk;
-  // 剩一堆时
-  if(stack.length === 1){
+
+  if (stack.length === 1) {
+    // 剩一堆时直接拿走
     return {
       best: { index: 0, num: stack[0] },
       nobest: { index: 0, num: stack[0] },
     }
   } else {
-    return {
-      best: { index: 0, num: 1 },
-      nobest: { index: 0, num: 1 },
+    // 罗列100条当前玩家赢的路径，路径短最优，其次为次优
+    /**
+     * actionArr结果实例，数组第一项中记录了该路径下一节点的行动方式
+     * getResult([[4,6,9]], [])
+     * actionArr = [
+        "[[4,9,{\"index\":0,\"num\":1}],[3,9],[2,9],[1,9],[1,8],[1,7],[1,6],[1,5],[1,4],[1,3]]",
+        "[[4,9,{\"index\":0,\"num\":1}],[3,9],[2,9],[1,9],[1,6],[1,3]]"
+        ...
+     * ]
+     */
+    let actionArr = getResult(stack, [], []);
+    if (actionArr.length === 0) {
+      // 若无必胜路径
+      return {
+        best: { index: 0, num: 1 },
+        nobest: { index: 0, num: 1 },
+      }
+    } else {
+      let bestRoute = actionArr[0]
+      let nobestRoute = actionArr[0]
+      actionArr.forEach((i: any) => {
+        let bastLen = JSON.parse(bestRoute).length;
+        let nobestLen = JSON.parse(nobestRoute).length;
+        let len = JSON.parse(i).length;
+        if (len < bastLen) {
+          bestRoute = i;
+        }
+        if (len > nobestLen) {
+          nobestRoute = i;
+        }
+      })
+      return {
+        best: JSON.parse(bestRoute)[0][JSON.parse(bestRoute)[0].length - 1],
+        nobest: JSON.parse(nobestRoute)[0][JSON.parse(bestRoute)[0].length - 1],
+      }
     }
   }
 }
+
+// 必赢节点
+const successPoint = [
+  '[1,1]', '[1,3]', '[2,2]', '[3,1]', '[3,3]', '[1,1,1,1]'
+]
+// 必输节点
+const failPoint = [
+  '[1,1,1]', '[1,1,2]', '[2,1,1]', '[1,2,1]'
+]
+
+const getResult = (arr:Array<number>, appendArr:Array<any>, resArr:Array<any>) => {
+  if (!appendArr.length) appendArr.push(arr);
+  let len = arr.length
+  let res = getStep(arr)
+  let array = resArr
+  res.forEach(ii => {
+    let i = ii.node;
+    let a = appendArr.concat([i])
+    if (a.length === 2) {
+      a[0][len] = ii.action
+    }
+    if (array.length < 100) {
+      if (successPoint.indexOf(JSON.stringify(i)) > -1 && a.length % 2 === 0) {
+        // 命中必赢节点, 直接返回
+        array.push(JSON.stringify(a))
+      } else if (failPoint.indexOf(JSON.stringify(i)) > -1 && a.length % 2 === 0) {
+        // 命中必输节点, 跳过
+      } else {
+        if (i.length) {
+          getResult(i, a, resArr)
+        } else {
+          // 选择偶数步的路径（算上当前节点）
+          if (array.indexOf(JSON.stringify(a)) < 0 && a.length % 2 === 0) {
+            array.push(JSON.stringify(a))
+          }
+        }
+      }
+    }
+
+  })
+  return array
+}
+
+const getStep = (stack: Array<any>) => {
+  const step: any = {
+    1: [1],
+    2: [1, 2],
+    3: [1, 3],
+    4: [1, 2, 4],
+    5: [1, 5],
+    6: [1, 2, 3, 6],
+    7: [1, 7],
+    8: [1, 2, 4, 8],
+    9: [1, 3, 9],
+    10: [1, 2, 5, 10],
+    11: [1, 11],
+    12: [1, 2, 3, 4, 6, 12]
+  }
+  let arr = [] as any[];
+  stack.forEach((item, index) => {
+    let res = step[item];
+    res.forEach((r: number) => {
+      // @ts-ignore
+      let _arr: any = [].concat(stack);
+      _arr[index] = item - r;
+      if (
+        (item === r && stack.length === 1 && item === 1) ||
+        (item === r && stack.length === 2 && stack[0] === 1 && stack[1] === 1) ||
+        stack.length > 2 ||
+        item != r
+      ) {
+        let action = { index: index, num: r }
+        if (item === r) {
+          _arr.splice(index, 1)
+        }
+        arr.push({ node: _arr, action: action });
+      }
+    })
+  })
+  return arr;
+}
+
+
