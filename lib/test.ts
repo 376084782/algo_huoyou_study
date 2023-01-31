a90c6bb89967f3168bd172b8e9e3034837234995
mxy <mxy@yufu.fun>
2023-01-31 09:20:48 +0800

    fix: 2_5

--- lib/test.ts
+++ lib/test.ts
@@ -576,40 +576,40 @@ for (let i = 0; i < 10; i++) {
 //     }
 // }
 
-let test2_5 = new example2_5();
-let config2_5 = new GameConfig2_5();
-config2_5.borderSize = 2;
-let data = test2_5.getRiddle(config2_5);
-console.info(JSON.stringify(data))
+// let test2_5 = new example2_5();
+// let config2_5 = new GameConfig2_5();
+// config2_5.borderSize = 2;
+// let data = test2_5.getRiddle(config2_5);
+// console.info(JSON.stringify(data))
 
-let action = new GameAction2_5(new Position2_5(0,1), 1);
-console.info(JSON.stringify(action.getBodyPosition()), JSON.stringify(action.getTailPosition()))
+// let action = new GameAction2_5(new Position2_5(0,1), 1);
+// console.info(JSON.stringify(action.getBodyPosition()), JSON.stringify(action.getTailPosition()))
 
-console.info(data.doAction(action), JSON.stringify(data))
+// console.info(data.doAction(action), JSON.stringify(data))
 
-let action2 = new GameAction2_5(new Position2_5(1,1), 1);
-console.info(data.doAction(action2), JSON.stringify(data))
-action = new GameAction2_5(new Position2_5(2,1), 1);
-console.info(JSON.stringify(action.getBodyPosition()), JSON.stringify(action.getTailPosition()))
-console.info(data.doAction(action), JSON.stringify(data))
-action = new GameAction2_5(new Position2_5(1,4), 4);
-console.info(JSON.stringify(action.getBodyPosition()), JSON.stringify(action.getTailPosition()))
-console.info(data.doAction(action), JSON.stringify(data))
-action = new GameAction2_5(new Position2_5(2,4), 4);
-console.info(JSON.stringify(action.getBodyPosition()), JSON.stringify(action.getTailPosition()))
-console.info(data.doAction(action), JSON.stringify(data))
+// let action2 = new GameAction2_5(new Position2_5(1,1), 1);
+// console.info(data.doAction(action2), JSON.stringify(data))
+// action = new GameAction2_5(new Position2_5(2,1), 1);
+// console.info(JSON.stringify(action.getBodyPosition()), JSON.stringify(action.getTailPosition()))
+// console.info(data.doAction(action), JSON.stringify(data))
+// action = new GameAction2_5(new Position2_5(1,4), 4);
+// console.info(JSON.stringify(action.getBodyPosition()), JSON.stringify(action.getTailPosition()))
+// console.info(data.doAction(action), JSON.stringify(data))
+// action = new GameAction2_5(new Position2_5(2,4), 4);
+// console.info(JSON.stringify(action.getBodyPosition()), JSON.stringify(action.getTailPosition()))
+// console.info(data.doAction(action), JSON.stringify(data))
 
 
-// let data2 = test2_5.doAction(data, action)
-// console.info(JSON.stringify(data2))
-config2_5.borderSize = 3;
-data = test2_5.getRiddle(config2_5);
-for (let i = 0; i < 10; i++) {
-    let action = test2_5.getActionAuto(data)
-    console.info(action)
-    let result = data.doAction(action.best)
-    console.info(result, JSON.stringify(data))
-    if (result != 0) {
-        break
-    }
-}
+// // let data2 = test2_5.doAction(data, action)
+// // console.info(JSON.stringify(data2))
+// config2_5.borderSize = 3;
+// data = test2_5.getRiddle(config2_5);
+// for (let i = 0; i < 10; i++) {
+//     let action = test2_5.getActionAuto(data)
+//     console.info(action)
+//     let result = data.doAction(action.best)
+//     console.info(result, JSON.stringify(data))
+//     if (result != 0) {
+//         break
+//     }
+// }
