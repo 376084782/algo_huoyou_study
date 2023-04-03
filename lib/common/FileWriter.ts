489d1607bee67a633dc31f5c3472eb7663c10299
huoyouqly <qly@huoyou.com>
2023-04-03 10:56:36 +0800

    1

--- lib/common/FileWriter.ts
+++ lib/common/FileWriter.ts
@@ -2,14 +2,14 @@ const fs = require('fs')
 export class FileWriter {
   static setFile(url: string, cnt: string) {
     return new Promise((rsv, rej) => {
-      fs.writeFile(url, cnt, 'utf-8', (err: string, data: any) => {
-        if (!err) {
-          rsv(null)
-        } else {
-          rej(null)
-        }
-      })
-      // rsv(null)
+      // fs.writeFile(url, cnt, 'utf-8', (err: string, data: any) => {
+      //   if (!err) {
+      //     rsv(null)
+      //   } else {
+      //     rej(null)
+      //   }
+      // })
+      rsv(null)
     })
   }
 }
