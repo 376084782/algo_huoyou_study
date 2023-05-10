8885a89776b11e075f6f0a7f510c1e5e2d9d2406
qianliyun <376084782@qq.com>
2023-05-10 14:56:53 +0800

    1

--- lib/common/FileWriter.ts
+++ lib/common/FileWriter.ts
@@ -1,20 +1,20 @@
-const fs = require('fs')
+// const fs = require('fs')
 export class FileWriter {
   static setFile(url: string, cnt: string) {
     return new Promise((rsv, rej) => {
-      // rsv(null)
-      // return;
-      let lastGIdx = url.lastIndexOf('/');
-      let dir = url.slice(0, lastGIdx);
-      fs.mkdir(dir, (err: any) => {
-        fs.writeFile(url, cnt, 'utf-8', (err: string, data: any) => {
-          if (!err) {
-            rsv(null)
-          } else {
-            rej(null)
-          }
-        })
-      })
+      rsv(null)
+      return;
+      // let lastGIdx = url.lastIndexOf('/');
+      // let dir = url.slice(0, lastGIdx);
+      // fs.mkdir(dir, (err: any) => {
+      //   fs.writeFile(url, cnt, 'utf-8', (err: string, data: any) => {
+      //     if (!err) {
+      //       rsv(null)
+      //     } else {
+      //       rej(null)
+      //     }
+      //   })
+      // })
     })
   }
 }
