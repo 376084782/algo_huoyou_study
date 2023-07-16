fceebbea4b41d09367a1069d569efeb608a25184
qianliyun <376084782@qq.com>
2023-07-17 00:23:58 +0800

    1

--- lib/books_odd/module5_2/test.ts
+++ lib/books_odd/module5_2/test.ts
@@ -3,7 +3,7 @@ import { FileWriter } from "../../common/FileWriter";
 
 let ctr = new module5_2()
 function test() {
-    let desk = ctr.getRiddle()
+    let desk = { "player": 1, "pChess": [2, 2], "pStart": [2, 0], "pGoal": [7, 7], "xMax": 8, "yMax": 8, "typeSet": 1 }
     // desk.options = [2, 3, 5]
     let act = ctr.getActionAuto(desk);
     console.log(act);
@@ -30,7 +30,7 @@ async function writeQuesIn(mapAll: any, maxEachLev = 10) {
     for (let level = 1; level <= 3; level++) {
         let listQues = mapAll[level].slice(0, maxEachLev)
         console.log(`写入等级${level},数量：${listQues.length}`)
-        await FileWriter.setFile(`./train/3-1/level${level}.json`, JSON.stringify(listQues))
+        await FileWriter.setFile(`./train/5-2/level${level}.json`, JSON.stringify(listQues))
     }
 
 }
