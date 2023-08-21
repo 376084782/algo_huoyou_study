28d31f5dcd1008e886fafa9f8437d441c6fd1d2d
qianliyun <376084782@qq.com>
2023-08-21 16:51:45 +0800

    1

--- lib/books_odd/module7_4/test.ts
+++ lib/books_odd/module7_4/test.ts
@@ -3,14 +3,14 @@ import { FileWriter } from "../../common/FileWriter";
 
 let ctr = new module7_4()
 function test() {
-    let desk = ctr.getRiddle([]);
-    desk.desk = [[1, 1, 1, 8]]
-    let act = {
-        listCalculate: ['(', '1', '+', '1', '+', '1', ')', '*', '8'],
-        score: 0
-    }
-    let [f, deskNew] = ctr.doAction(desk, act);
-    console.log(f, deskNew)
+    let desk = {"player":1,"desk":[[5,6,7,8]],"curQuesIdx":1,"score1":-1,"score2":0,"typeSet":1}
+    // desk.desk = [[1, 1, 1, 8]]
+    // let act = {
+    //     listCalculate: ['(', '1', '+', '1', '+', '1', ')', '*', '8'],
+    //     score: 0
+    // }
+    let f = ctr.checkDesk(desk);
+    console.log(f)
 }
 
 test()
@@ -28,4 +28,4 @@ async function writeQuesIn(mapAll: any, maxEachLev = 10) {
     }
 
 }
-createQues()
+// createQues()
